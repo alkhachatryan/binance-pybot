963f37c293bf89d946a09d2e90241627c7c7f53b
Alexey <akhachatryan@bytechek.com>
2022-11-21 17:09:38 +0400

    Fixed couple of bugs

--- func.py
+++ func.py
@@ -1,3 +1,5 @@
+import sys
+
 from binance_utils import *
 from logger import log
 
@@ -22,30 +24,29 @@ def get_free_balance_of(coin):
 def create_sell_order(price_to_sell_with, coin_to_buy, symbol):
     coin_to_buy_balance = get_free_balance_of(coin_to_buy)
 
-    send_signed_request('POST', 'order', {
+    res = send_signed_request('POST', 'order', {
         'symbol': symbol,
         'side': 'sell',
         'type': 'limit',
-        'quantity': coin_to_buy_balance,
-        'price': price_to_sell_with,
+        'quantity': "%.5f" % coin_to_buy_balance,
+        'price': "%.5f" % price_to_sell_with,
         'timeInForce': 'GTC'
     })
+
+    # Got an error from Binance API
+    if 'code' in res:
+        log(res)
+        sys.exit(1)
+
     log('Created sell order ' + str(coin_to_buy_balance) + ' with price ' + str(price_to_sell_with))
 
 
-def create_buy_order(coin_to_buy, coin_to_sell, symbol):
-    coin_to_sell_balance = get_free_balance_of(coin_to_sell)
-
-    # In case if you sell USDT to buy another coin, transaction price should be more than $10
-    if coin_to_sell == 'USDT' and coin_to_sell_balance <= 10:
-        log('Not enough resources: ' + str(coin_to_sell_balance))
-
-    log('Current balance: ' + str(coin_to_sell_balance) + coin_to_sell)
+def create_buy_order(coin_to_buy, coin_to_sell, symbol, money_to_spend):
     current_price = get_symbol_price(symbol)
-    money_to_spend = coin_to_sell_balance / 2
     qty_to_buy = money_to_spend / current_price
     qty_to_buy = "%.5f" % qty_to_buy
-    response = send_signed_request('POST', 'order', {'symbol': symbol, 'side': 'buy', 'type': 'market', 'quantity': qty_to_buy})
+    response = send_signed_request('POST', 'order',
+                                   {'symbol': symbol, 'side': 'buy', 'type': 'market', 'quantity': qty_to_buy})
 
     bought_with_price = float(response['fills'][0]['price'])
     qty_bought = response['fills'][0]['qty']
--- main.py
+++ main.py
@@ -4,18 +4,40 @@
 from cli_args_parser import cli_args
 from func import *
 
+current_balance = 0
+
 
 def trade(coin_to_buy, coin_to_sell):
+    global current_balance
     symbol = coin_to_buy + coin_to_sell
 
     while True:
         no_open_order = len(get_open_orders(symbol)) == 0
 
-        # After hours of tests the best limitation in second between orders creations
-        time.sleep(4)
-
         if no_open_order:
-            create_buy_order(coin_to_buy, coin_to_sell, symbol)
+            # After hours of tests the best limitation in second between orders creations
+            time.sleep(2)
+
+            coin_to_sell_balance = get_free_balance_of(coin_to_sell)
+
+            # Sometimes Binance API doesn't provide a correct balance after completed order
+            # When your order completed Binance shows old balance
+            # In that case wait additional second before continuing the trading process
+            if current_balance > coin_to_sell_balance:
+                log('Old balance returned from API, waiting a second')
+                time.sleep(1)
+                return trade(coin_to_buy, coin_to_sell)
+
+            current_balance = coin_to_sell_balance
+
+            # In case if you sell USDT to buy another coin, transaction price should be more than $10
+            if coin_to_sell == 'USDT' and coin_to_sell_balance <= 10:
+                log('Not enough resources: ' + str(coin_to_sell_balance))
+
+            log('Current balance: ' + str(coin_to_sell_balance) + coin_to_sell)
+            money_to_spend = coin_to_sell_balance / 2
+
+            create_buy_order(coin_to_buy, coin_to_sell, symbol, money_to_spend)
 
 
 def main():
